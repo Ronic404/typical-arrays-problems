2cbb1a079f921a4888514587cf080b978d55635a
ROnic404 <ronic@inbox.ru>
2020-02-22 12:43:26 +0400

    feat: add solution task typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = []) {
+    return (array.length<1) ? 0 : Math.min(...array); 
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array = []) {
+    return (array.length<1) ? 0 : Math.max(...array);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array = []) {
+    let sum = 0;
+    for(let i of array) {
+        sum += i;
+    }
+    return (array.length<1) ? 0 : sum/array.length; 
 }
